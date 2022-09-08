a779c21521bba2ce0a926c8bdfd6142877a41d29
Taps B <63951156+taps098@users.noreply.github.com>
2022-09-08 16:10:30 +0200

    indentation change

--- src/components/ProfileDetails.js
+++ src/components/ProfileDetails.js
@@ -16,88 +16,83 @@ import bank from '../extra/bank2.svg';
  * @returns profileDetails
  */
 const ProfileDetails = () => {
-	const profileDetails = useRecoilValue(userProfileDetails);
-	const setNameInState = useSetRecoilState(userProfileDetails);
-	const [userName, setUserName] = useState(profileDetails.userName);
-	const [isEditMode, setIsEditMod] = useState(false);
-	/**
-	* function to state of Edit
-	*/
-	const handleEdit = useCallback(() => {
-		setIsEditMod((oldValue) => !oldValue);
-	}, []);
+    const profileDetails = useRecoilValue(userProfileDetails);
+    const setNameInState = useSetRecoilState(userProfileDetails);
+    const [userName, setUserName] = useState(profileDetails.userName);
+    const [isEditMode, setIsEditMod] = useState(false);
+    /**
+    * function to state of Edit
+    */
+    const handleEdit = useCallback(() => {
+        setIsEditMod((oldValue) => !oldValue);
+    }, []);
 
-	/**
-	 * function to handle the name change
-	 */
-	// const handleNameChange = (e) => {
-	// 	e.preventDefault();
-	// 	setUserName(e.target.value)
-	// }
-	const handleNameChange = useCallback((e) => {
-		e.preventDefault();
-		setUserName(e.target.value)
-	}, []);
+    /**
+     * function to handle the name change
+     */
+    const handleNameChange = useCallback((e) => {
+        setUserName(e.target.value)
+    }, []);
 
-	/**
-	 * handle save button functionality
-	 * to save the data in the store
-	 */
-	const handleSave = useCallback(() => {
-		setNameInState(
-			{
-				...profileDetails,
-				userName,
-			});
-		setIsEditMod(!isEditMode);
-	}, [userName, isEditMode]);
+    /**
+     * handle save button functionality
+     * to save the data in the store
+     */
+    const handleSave = useCallback(() => {
+        setNameInState(
+            {
+                ...profileDetails,
+                userName,
+            });
+        setIsEditMod(false);
+    }, [userName, isEditMode]);
 
-	return (
-		<div className="user-details">
-			<div className="user-profileDetails">
-				<div className="user-name-content">
-					<div className="user-name">
-						{
-							isEditMode ?
-								<input className="nameInputStyle" data-testid='nameInput' value={userName} onChange={handleNameChange} />
-								: <div data-testid='nameField'>{profileDetails.userName}</div>
-						}
-					</div>
-					<div>
-						{
-							isEditMode ? <button data-testid='save-button' className="save-button" onClick={handleSave}>Save</button>
-								: <img data-testid='edit-button' src={pencil} alt='img' onClick={handleEdit} class="bi bi-pencil" />
-						}
-					</div>
-				</div>
-				<div className="user-details-content">
-					<img src={envelope} alt='img' className="bi bi-envelope" />
+    return (
+        <div className="user-details">
+            <div className="user-profileDetails">
+                <div className="user-name-content">
+                    <div className="user-name">
+                        {
+                            isEditMode ?
+                                <input className="nameInputStyle" data-testid='nameInput' value={userName} onChange={handleNameChange} />
+                                : <div data-testid='nameField'>{profileDetails.userName}</div>
+                        }
+                    </div>
+                    <div>
+                        {
+                            isEditMode ? <button data-testid='save-button' className="save-button" onClick={handleSave}>Save</button>
+                                : <img data-testid='edit-button' src={pencil} alt='img' onClick={handleEdit} class="bi bi-pencil" />
+                        }
+                    </div>
+                </div>
+                <div className="user-details-content">
+                    <img src={envelope} alt='img' className="bi bi-envelope" />
 
-					<div className="profile-line">{profileDetails.mailId}</div>
-				</div>
-				<div className="user-details-content">
-					<div><img src={filePerson} alt='img' class="bi bi-file-person" /></div>
-					<div className="profile-line">{profileDetails.address}</div>
-				</div>
-				<div className="user-details-content">
-					<div><img src={telephone} alt='img' class="bi bi-file-person" /></div>
-					<div className="profile-line">{profileDetails.phoneNumber}</div>
-				</div>
-				<div className="user-details-content">
-					<div><img src={globe} alt='img' class="bi bi-file-person" /></div>
-					<div className="profile-line">{profileDetails.org}</div>
-				</div>
-			</div>
-			<div className="company-logo">
-				<div ><img src={bank} alt='img' class="bi bi-file-person" /></div>
-				<div className="profile-line">
-					<div className="company-name"> McKenzie LLC</div>
-					<div className="company-statment">Quality focused value-added synergy</div>
-				</div>
-			</div>
+                    <div className="profile-line">{profileDetails.mailId}</div>
+                </div>
+                <div className="user-details-content">
+                    <div><img src={filePerson} alt='img' class="bi bi-file-person" /></div>
+                    <div className="profile-line">{profileDetails.address}</div>
+                </div>
+                <div className="user-details-content">
+                    <div><img src={telephone} alt='img' class="bi bi-file-person" /></div>
+                    <div className="profile-line">{profileDetails.phoneNumber}</div>
+                </div>
+                <div className="user-details-content">
+                    <div><img src={globe} alt='img' class="bi bi-file-person" /></div>
+                    <div className="profile-line">{profileDetails.org}</div>
+                </div>
+            </div>
+            <div className="company-logo">
+                <div ><img src={bank} alt='img' class="bi bi-file-person" /></div>
+                <div className="profile-line">
+                    <div className="company-name"> McKenzie LLC</div>
+                    <div className="company-statment">Quality focused value-added synergy</div>
+                </div>
+            </div>
 
-		</div>
-	);
+        </div>
+    );
 };
 
 export default ProfileDetails;
